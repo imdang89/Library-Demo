4bba9ed1b88c540cd7eeb4000ca537f5d197d20e
imdang89 <dang.lh0809@gmail.com>
2023-05-18 16:01:43 +0700

    second__

---
+++ build/CMakeFiles/library.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/imdang89/code/Library-Demo/source/EmployeeFT.cpp" "/home/imdang89/code/Library-Demo/build/CMakeFiles/library.dir/source/EmployeeFT.cpp.o"
+  "/home/imdang89/code/Library-Demo/source/EmployeePT.cpp" "/home/imdang89/code/Library-Demo/build/CMakeFiles/library.dir/source/EmployeePT.cpp.o"
+  "/home/imdang89/code/Library-Demo/source/Person.cpp" "/home/imdang89/code/Library-Demo/build/CMakeFiles/library.dir/source/Person.cpp.o"
+  "/home/imdang89/code/Library-Demo/source/main.cpp" "/home/imdang89/code/Library-Demo/build/CMakeFiles/library.dir/source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/library.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/library.dir/source/EmployeeFT.cpp.o"
+  "CMakeFiles/library.dir/source/EmployeePT.cpp.o"
+  "CMakeFiles/library.dir/source/Person.cpp.o"
+  "CMakeFiles/library.dir/source/main.cpp.o"
+  "library"
+  "library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
